2fb4fab68d8b9df27e6d3dfe16e5ded57ba3df48
DonRumata710 <donrumata710@yandex.com>
2021-07-08 16:01:22 +0300

    codestyle fixes

--- ReactClient/src/pages/home.js
+++ ReactClient/src/pages/home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import Header from '../components/header'
 import { signoutRedirect } from '../services/userService'
 import { useSelector } from 'react-redux'
