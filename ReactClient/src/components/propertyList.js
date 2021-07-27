19ffe27939e4ec5d724262b9032154f84827de70
DonRumata710 <donrumata710@yandex.com>
2021-07-28 00:59:11 +0300

    add property list component

---
+++ ReactClient/src/components/propertyList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class PropertyList extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.propertyFields = this.props.properties.map((prop) => {
+            return (<div key={prop.name}>{prop.name} <input type={prop.type} defaultValue={prop.currentValue} onChange={(input) => { prop.currentValue = input.target.value; }}></input></div>)
+        });
+    }
+
+    render() {
+        return (
+            <div className="property-list">
+                {this.propertyFields}
+            </div>
+        )
+    }
+}
+
+export default PropertyList;
